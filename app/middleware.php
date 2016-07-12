c50cb94687375b95eafbfe50e04ced2179a9355e
Dylan Socolobsky <dsocolobsky@gmail.com>
2016-07-12 16:46:33 -0300

    Arreglar error con 'id' empty

--- app/middleware.php
+++ app/middleware.php
@@ -1,17 +1,17 @@
 <?php
 
 $index = function($request, $response, $next) {
-    if (!isset($_SESSION['id']) || empty($_SESSION['id'])) {
+    if (isEmpty()) {
         return $response->withStatus(302)->withHeader('Location', '/login');
     } else if ($_SESSION['id'] == 'admin') {
         return $response->withStatus(302)->withHeader('Location', '/pedidos');
-    } else if (isset($_SESSION['id']) || !empty($_SESSION['id'])) {
+    } else if (isSomething()) {
         return $response->withStatus(302)->withHeader('Location', '/menu');        
     }
 };
 
 $debeLoggearse = function($request, $response, $next) {
-    if (!isset($_SESSION['id']) || empty($_SESSION['id'])) {
+    if (isEmpty()) {
         return $response->withStatus(302)->withHeader('Location', '/login');
     }
 
@@ -27,12 +27,20 @@ $noDebeLoggearse = function($request, $response, $next) {
 };
 
 $debeSerAdmin = function($request, $response, $next) {
-    if ($_SESSION['id'] != 'admin') {
+    if (isSomething() && $_SESSION['id'] != 'admin') {
         return $response->withStatus(302)->withHeader('Location', '/');
     }
 
     return $next($request, $response);
 };
+
+function isEmpty() {
+    return !isset($_SESSION['id']) || empty($_SESSION['id']);
+}
+
+function isSomething() {
+    return isset($_SESSION['id']) && !empty($_SESSION['id']);
+}
 
 
 ?>
