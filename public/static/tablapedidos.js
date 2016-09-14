49586f4eb954124058153e1892d4cec6b979411d
Dylan Socolobsky <dsocolobsky@gmail.com>
2016-09-14 16:40:51 -0300

    Arreglar la entrega de pedidos

--- public/static/tablapedidos.js
+++ public/static/tablapedidos.js
@@ -1,20 +1,11 @@
-$("#tabla_entregados > table > tbody > tr").each(function (i, row) {
+$("#tabla_pendientes > table > tbody > tr").each(function (i, row) {
     var button = $(this).find("td").find("button");
     button.unbind('click').bind('click', function () {
         var id = $(this).attr("name").replace("pedido", "");
 
-        $.post("/borrarpedido", { id: id })
+        $.post("/listo", { id: id })
             .done(function (data) {
                 window.location.replace("/pedidos");
             });
     });
 });
-
-function cargarTabla() {
-    $("#tabla_pendientes").load("/tablapedidos");
-}
-
-cargarTabla();
-setInterval( function() {
-    cargarTabla();
-}, 8000);
